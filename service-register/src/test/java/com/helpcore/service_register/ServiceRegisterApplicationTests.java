9d3a5f8b4d5826961633621332398ea91c5a1a68
Carlos <arenasgarrocarlos@gmail.com>
2025-09-18 23:14:28 -0500

    eureka configuration (auth-service)

---
+++ service-register/src/test/java/com/helpcore/service_register/ServiceRegisterApplicationTests.java
@@ -0,0 +1,14 @@
+package com.helpcore.service_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class ServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
