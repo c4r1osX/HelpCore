f7086e6efb4850c92c75b7e9dbecc4f1e63cc4b5
Joaquin Suarez <joaquin.asr.16@gmail.com>
2025-09-22 00:54:32 -0500

    crear dto de ticket creado y servicio para la creacion de emails

---
+++ notification-service/src/main/java/com/helpcore/notification_service/dto/TicketCreatedDto.java
@@ -0,0 +1,10 @@
+package com.helpcore.notification_service.dto;
+
+import lombok.Data;
+
+public class TicketCreatedDto {
+    private Long ticketId;
+    private String titulo;
+    private String descripcion;
+    private String creadoPor;
+}
